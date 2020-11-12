f23450da96a869297554fe1395b42315cfcca59c
Damir <d.fatkhullin@mail.ru>
2020-11-12 17:44:40 +0300

    The registration window is connected to the SQL database

--- RegForm.Designer.cs
+++ RegForm.Designer.cs
@@ -36,6 +36,8 @@
             this.rfPass = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.namebox = new System.Windows.Forms.TextBox();
+            this.surnamebox = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
@@ -47,7 +49,7 @@
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(38)))), ((int)(((byte)(148)))));
             this.label1.Location = new System.Drawing.Point(-2, -2);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(348, 81);
+            this.label1.Size = new System.Drawing.Size(728, 81);
             this.label1.TabIndex = 0;
             this.label1.Text = "Регистрация";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -57,7 +59,7 @@
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
-            this.pictureBox1.Location = new System.Drawing.Point(12, 93);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 166);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(58, 55);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -68,7 +70,7 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(12, 171);
+            this.pictureBox2.Location = new System.Drawing.Point(404, 166);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(58, 50);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -80,7 +82,7 @@
             // 
             this.rfLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(128)))), ((int)(((byte)(178)))));
             this.rfLogin.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rfLogin.Location = new System.Drawing.Point(84, 93);
+            this.rfLogin.Location = new System.Drawing.Point(76, 166);
             this.rfLogin.Multiline = true;
             this.rfLogin.Name = "rfLogin";
             this.rfLogin.Size = new System.Drawing.Size(248, 55);
@@ -90,7 +92,7 @@
             // 
             this.rfPass.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(128)))), ((int)(((byte)(178)))));
             this.rfPass.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rfPass.Location = new System.Drawing.Point(84, 171);
+            this.rfPass.Location = new System.Drawing.Point(468, 166);
             this.rfPass.Name = "rfPass";
             this.rfPass.Size = new System.Drawing.Size(248, 48);
             this.rfPass.TabIndex = 5;
@@ -101,20 +103,22 @@
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(70)))), ((int)(((byte)(157)))));
             this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(38)))), ((int)(((byte)(148)))));
-            this.button1.Location = new System.Drawing.Point(97, 253);
+            this.button1.Location = new System.Drawing.Point(222, 244);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(160, 32);
+            this.button1.Size = new System.Drawing.Size(273, 46);
             this.button1.TabIndex = 6;
             this.button1.Text = "Зарегистрироваться";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label2
             // 
             this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(36)))), ((int)(((byte)(147)))));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(38)))), ((int)(((byte)(148)))));
-            this.label2.Location = new System.Drawing.Point(324, -2);
+            this.label2.Location = new System.Drawing.Point(700, -2);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(22, 17);
+            this.label2.Size = new System.Drawing.Size(26, 24);
             this.label2.TabIndex = 7;
             this.label2.Text = "x";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -122,13 +126,41 @@
             this.label2.MouseEnter += new System.EventHandler(this.label2_MouseEnter);
             this.label2.MouseLeave += new System.EventHandler(this.label2_MouseLeave);
             // 
+            // namebox
+            // 
+            this.namebox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(128)))), ((int)(((byte)(178)))));
+            this.namebox.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.namebox.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.namebox.Location = new System.Drawing.Point(76, 91);
+            this.namebox.Multiline = true;
+            this.namebox.Name = "namebox";
+            this.namebox.Size = new System.Drawing.Size(248, 55);
+            this.namebox.TabIndex = 8;
+            this.namebox.Enter += new System.EventHandler(this.namebox_Enter);
+            this.namebox.Leave += new System.EventHandler(this.namebox_Leave);
+            // 
+            // surnamebox
+            // 
+            this.surnamebox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(128)))), ((int)(((byte)(178)))));
+            this.surnamebox.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.surnamebox.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.surnamebox.Location = new System.Drawing.Point(468, 91);
+            this.surnamebox.Multiline = true;
+            this.surnamebox.Name = "surnamebox";
+            this.surnamebox.Size = new System.Drawing.Size(248, 55);
+            this.surnamebox.TabIndex = 9;
+            this.surnamebox.Enter += new System.EventHandler(this.surnamebox_Enter);
+            this.surnamebox.Leave += new System.EventHandler(this.surnamebox_Leave);
+            // 
             // RegForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(70)))), ((int)(((byte)(157)))));
-            this.ClientSize = new System.Drawing.Size(344, 324);
+            this.ClientSize = new System.Drawing.Size(728, 324);
             this.ControlBox = false;
+            this.Controls.Add(this.surnamebox);
+            this.Controls.Add(this.namebox);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.rfPass);
@@ -156,5 +188,7 @@
         private System.Windows.Forms.TextBox rfPass;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox namebox;
+        private System.Windows.Forms.TextBox surnamebox;
     }
 }
--- RegForm.cs
+++ RegForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,12 @@ namespace example
         public RegForm()
         {
             InitializeComponent();
+
+            namebox.Text = "Введите имя";
+            namebox.ForeColor = Color.Gray;
+
+            surnamebox.Text = "Введите фамилию";
+            surnamebox.ForeColor = Color.Gray;
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -68,6 +75,92 @@ namespace example
         private void label2_MouseLeave(object sender, EventArgs e)
         {
             label2.ForeColor = Color.Blue;
+        }
+
+        private void namebox_Enter(object sender, EventArgs e)
+        {
+            if (namebox.Text == "Введите имя")
+            {
+                namebox.Text = " ";
+               
+            }
+        }
+
+        private void surnamebox_Enter(object sender, EventArgs e)
+        {
+            if (surnamebox.Text == "Введите фамилию")
+            {
+                surnamebox.Text = " ";
+               
+            }
+        }
+
+        private void namebox_Leave(object sender, EventArgs e)
+        {
+            if (namebox.Text == " ")
+            {
+                namebox.Text = "Введите имя";
+            }
+        }
+
+        private void surnamebox_Leave(object sender, EventArgs e)
+        {
+            if (surnamebox.Text == " ")
+            {
+                surnamebox.Text = "Введите фамилию";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (namebox.Text == "Введите имя" || namebox.Text == " " || surnamebox.Text == " " || surnamebox.Text == "Введите фамилию")
+            {
+                MessageBox.Show("Введите данные!");
+                return;
+            }
+
+            if (checkName())
+                return;
+            
+            DB db = new DB();
+            MySqlCommand command = new MySqlCommand("INSERT INTO `users` (`login`, `password`, `name`, `surname`) VALUES (@login, @pass, @name, @surname);", db.getConnection());
+            command.Parameters.Add ("@login", MySqlDbType.VarChar).Value = rfLogin.Text;
+            command.Parameters.Add("@pass", MySqlDbType.VarChar).Value = rfPass.Text;
+            command.Parameters.Add("@name", MySqlDbType.VarChar).Value = namebox.Text;
+            command.Parameters.Add("@surname", MySqlDbType.VarChar).Value = surnamebox.Text;
+
+            db.openCon();
+
+            if (command.ExecuteNonQuery() == 1)
+                MessageBox.Show("Аккаунт зарегестрирован");
+            else MessageBox.Show("Ошибка.Аккаунт не зарегестрирован");
+
+            db.closeCon();
+        }
+        public Boolean checkName()
+        {
+            DB db = new DB();
+
+            DataTable table = new DataTable();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+            MySqlCommand command = new MySqlCommand("SELECT * FROM `users` WHERE `login` = @ul ", db.getConnection());
+            command.Parameters.Add("@ul", MySqlDbType.VarChar).Value = rfLogin.Text;
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if (table.Rows.Count > 0)
+            {
+                MessageBox.Show("Такой пользователь существует");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
         }
     }
 }
